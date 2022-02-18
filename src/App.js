dc7e6ba9043cf3f93e6eb51103ba4c41931710c1
rohit katbamna <rohitkatbamna@protonmail.com>
2022-02-18 11:36:56 +0530

    18TH FEB 11:36 AM  1ST COMMIT

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [inputvalue, setInputvalue] = useState(NaN);
+	const [numberequal, setNumberequal] = useState(false);
+	const [randomvalue, setRandomvalue] = useState(0);
+	let inputedNumber;
+	function handleChange(event) {
+		inputedNumber = event.target.value;
+		setInputvalue(inputedNumber);
+	}
+	function onSubmit() {
+		setRandomvalue(Math.floor(Math.random() * 2));
+
+		if (Number(inputvalue) === randomvalue) {
+			setNumberequal(true);
+		} else {
+			setNumberequal(false);
+		}
+	}
+
+	return (
+		<div className="container-fluid">
+			<div className="row">
+				<div className="col-12 text-center">
+					<input
+						type="number"
+						style={{
+							width: "10vw",
+							height: "5vw",
+							fontSize: "30px",
+							marginTop: "5vw",
+						}}
+						onChange={handleChange}
+					/>
+					<br />
+					<button className="btn btn-primary btn-lg mt-5" onClick={onSubmit}>
+						Submit
+					</button>
+					<p className="fs-1 text-dark">Inputed number : {inputvalue}</p>
+					<p className="fs-1">Random number : {randomvalue}</p>
+					<p className="mt-5 fs-1">
+						{" "}
+						Is the number you input equal to guessed number
+						<br />
+					</p>
+					<h1>{numberequal ? "yes it is" : "No it not"}</h1>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
